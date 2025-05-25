672f28e12e561332f338b7b34e3174693c95fa71
Anantha <anantha@anr3.local>
2025-05-24 19:36:46 -0700

    Added a Board class with some CLI display stuff

---
+++ Board.py
@@ -0,0 +1,65 @@
+import re,sys
+import numpy as np
+
+
+
+class Board:
+    """
+    
+    """
+
+    SPACING = "  "
+    PLAYER_X_NUMBER = 1
+    PLAYER_O_NUMBER = 2
+
+    def __init__(self):
+        """
+        
+        """
+        self.board = np.full((3,3),0,dtype=int)
+
+    def __repr__(self):
+        str_mat = [["" for _ in range(self.board.shape[0])] for _ in range(self.board.shape[1])]        
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                str_mat[i][j] = str(self.board[i][j])
+        
+        return "\n".join([Board.SPACING.join(cell) for cell in str_mat])
+
+    def move(self,cell:tuple[int,int],playerX:bool = True) -> None:
+        """Put a move on the board
+            Arguments:
+                cell: coordinates of where to place the move
+                playerX: indicates whether to place an 'X' or 'O'
+            
+            Returns:
+                None
+        """
+        row = cell[0]
+        col = cell[1]
+        if self.board[row][col] != 0:
+            print("Invalid Move")
+            return
+        
+        self.board[row][col] = Board.PLAYER_X_NUMBER if playerX else Board.PLAYER_O_NUMBER
+
+    
+    def print(self):
+        """
+        
+        """
+        str_mat = [["" for _ in range(self.board.shape[0])] for _ in range(self.board.shape[1])]        
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                symb = ""
+                if self.board[i][j] == Board.PLAYER_X_NUMBER:
+                    symb = "X"
+                elif self.board[i][j] == Board.PLAYER_O_NUMBER:
+                    symb = "O"
+                else:
+                    symb = "-"
+                str_mat[i][j] = symb
+        
+        print("\n".join([Board.SPACING.join(cell) for cell in str_mat]))
+
+        
