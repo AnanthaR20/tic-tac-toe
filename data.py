647a6e479a6eba2847e9e4c2f6e7be3c6a06269b
Anantha <anantha@Mac.lan>
2025-07-09 20:21:21 -0500

    Rigged up the Dataset class so that things can be proproperly be batched as tensors etc. Next step is to write the training script. Use the hw4 script as a reference.

--- data.py
+++ data.py
@@ -1,16 +1,54 @@
 import re,sys,random
+from typing import Any
 import numpy as np
 import pandas as pd
 
-label_splitter = re.compile(r"[\d-]+")
-feature_splitter = re.compile(r"[^,]")
+# Type Alias
+Example = dict[str, Any]
 
 class Dataset:
+    def __init__(self, examples: list[Example]) -> None:
+        self.examples = examples
 
-    def __init__(self,x,y):
-        self.features = x
-        self.label = y
-        self.data = [x,y]
+    def example_to_tensors(self, index: int) -> dict[str, np.ndarray]:
+        raise NotImplementedError
+
+    def batch_as_tensors(self, start: int, end: int) -> dict[str, np.ndarray]:
+        raise NotImplementedError
+
+    def __getitem__(self, idx: int) -> Example:
+        return self.examples[idx]
+
+    def __len__(self) -> int:
+        return len(self.examples)
+    
+
+class TTTDataset(Dataset):
+
+    labels_to_move = {0: (0,0), 1: (0,1), 2: (0,2), 3: (1,0), 4: (1,1), 5:(1,2), 6:(2,0), 7:(2,1), 8:(2,2)}
+    label_one_hots = np.eye(len(labels_to_move))
+
+    def example_to_tensors(self, index:int) -> dict[str,np.ndarray]:
+        """Convert a given example to Tensor format
+        
+        Args:
+            index: desired example
+        
+        Returns:
+            The same example, but in Tensor form and with one-hot vector for label
+        """
+        example = self.__getitem__(index)
+        return {
+            "input": np.array(example["input"]),
+            "label": TTTDataset.label_one_hots[example["label"]]
+        }
+    
+    def batch_as_tensors(self, start: int, end: int) -> dict[str, np.ndarray]:
+        examples = [self.example_to_tensors(index) for index in range(start, end)]
+        return {
+            "input": np.stack([example["input"] for example in examples]),
+            "label": np.stack([example["label"] for example in examples]),
+        }
 
     def from_txt(file:str):
         """Return a Dataset from a text file.
@@ -18,6 +56,8 @@ class Dataset:
         Args:
             file: filename
         """
+        label_splitter = re.compile(r"[\d-]+")
+        # feature_splitter = re.compile(r"[^,]")
         l = []
         
         with open(file, 'r') as f:
@@ -25,13 +65,14 @@ class Dataset:
             for i in file_as_list:
                 label = int(label_splitter.findall(i)[0])
                 features = label_splitter.findall(i)[1:]
-                print(label_splitter.findall(i))
-                print(features)
+                # print(label_splitter.findall(i))
+                # print(features)
                 features = [int(j) for j in features]
 
-                l.append((features,label))
+                l.append({
+                    "input": features,
+                    "label": label
+                })
         
-        return l
-
-
+        return TTTDataset(examples=l)
 
